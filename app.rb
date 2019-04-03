a50c3c01f36a19cb79e532abf7f10449af15052c
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:03:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/reversename/:name' do
+  get '/reversename/:name' do |name|
     @name = params[:name]
     @name.reverse
   end
-  
+
 end
