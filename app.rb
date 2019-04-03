0dff85a2221ac64d1262f52170b83c622af3bcd6
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:55:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App < Sinatra::Base
     @num2 = (params[:number2]).to_i
     if @oper = "add"
       @num1 + @num2
-    if @oper = "subtract"
+    elsif @oper = "subtract"
       return @num1 - @num2
     when @oper = "multiply"
       return @num1 * @num2
