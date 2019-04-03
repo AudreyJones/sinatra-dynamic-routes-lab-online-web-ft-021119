13cd9c5f21c301840137a417bee90247e736a74c
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:50:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,14 +31,23 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
-  get '/:operation/:number1/:number2'
-  end
-  do |oper,num1,num2|
+  get '/:operation/:number1/:number2' do |oper,num1,num2|
     binding.pry
     @oper = params[:operation]
     @num1 = (params[:number1]).to_i
     @num2 = (params[:number2]).to_i
 
+    case @oper
+    when @oper = "add"
+      @num1 + @num2
+    when @oper = "subtract"
+      @num1 - @num2
+    when @oper = "multiply"
+      @num1 * @num2
+    when @oper = "divide"
+      @num1/@num2
+      
+      
 
     @num1(@oper)@num2
   end
