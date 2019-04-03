e4dfecd3cc7b95f3e51ba6631afca3e5c0655f0e
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:39:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,7 +31,8 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
-  get '/:operation/:number1/:number2' do
-    @word1 = params[:word1]
+  get '/:operation/:number1/:number2' do |num1,num2|
+    @num1 = params[:number1]
+    @num2 = params[:number2]
   end
 end
