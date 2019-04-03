0d6e9e66a4a9a3d884cc27ad6796747b910fb427
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:59:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -39,9 +39,7 @@ class App < Sinatra::Base
     if @oper == "add"
       @result = @num1 + @num2
     elsif @oper == "subtract"
-
       @result = @num1-@num2
-
     elsif @oper == "multiply"
       @result = @num1 * @num2
     elsif @oper == "divide"
