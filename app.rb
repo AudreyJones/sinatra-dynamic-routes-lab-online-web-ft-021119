83257ffd350a51ff8dd665c8f0e42a93af9154f5
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:38:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,10 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    # binding.pry
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
   end
 end
