f3b8b912e41522390a263c68d437b11fb60fa573
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:33:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do |number,phrase|
     @phrase = params[:phrase]
     @number = (params[:number]).to_i
-  
+
     @empty = ""
     @number.times do
         binding.pry
