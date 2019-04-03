7daa62180f45b8b63b38428d9023cf02e8381608
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-03 15:40:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,8 +33,9 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do |oper,num1,num2|
     @oper = params[:operation]
+    binding.pry
     @num1 = (params[:number1]).to_i
     @num2 = (params[:number2]).to_i
-    
+
   end
 end
